24fc41301220099927915ca8e8584e7fbcfa0b15
Aljoscha Nyang <a.nyang@gmx.de>
2023-11-12 14:07:31 +0100

    Add getAllTodos method and GetMapping

--- backend/src/main/java/de/aljoschanyang/backend/controller/TodoController.java
+++ backend/src/main/java/de/aljoschanyang/backend/controller/TodoController.java
@@ -1,13 +1,20 @@
 package de.aljoschanyang.backend.controller;
 
+import de.aljoschanyang.backend.model.Todo;
 import de.aljoschanyang.backend.service.TodoService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/todos")
 public class TodoController {
     private TodoService todoService;
+
+    @GetMapping("")
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
 }
